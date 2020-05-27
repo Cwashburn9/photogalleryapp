faeccb5e5e188204c69a4ba0f810f2db072f7d4a
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-05-27 15:44:04 -0500

    added storage and mobile

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,6 +9,10 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) {}
-
+  constructor(public photoService: PhotoService) { }
+  
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
 }
+
